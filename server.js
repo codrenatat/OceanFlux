7a315a935d87dfbc798a4a689c4ad39ec79e5573
codrenatat <codrenatat@gmail.com>
2023-11-17 21:53:00 -0600

    La neta ni se que hice

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const mongoose = require("mongoose");
+/*
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+/*MONGOOSE*/
+/*
 let mongoConnection = "mongodb+srv://admin:oceanflux@project.l7yrx7s.mongodb.net/";
 let db = mongoose.connection;
 
@@ -13,8 +24,110 @@ db.on('connected', () => {
     console.log(mongoose.connection.readyState);
 });
 
-mongoose.connect(mongoConnection, {useNewUrlParser: true, useUnifiedTopology: true});
+database.on('error', (err) => {
+    console.error('ERROR!!', err);
+});
 
+mongoose.connect(mongoConnection, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+/*SCHEMA*/
+/*
+let userSchema = mongoose.Schema({
+    nombre:{
+        type: String,
+        required: true
+    },
+    correo:{
+        type:String,
+        required: true
+    },
+    contrasenia: {
+        type: String,
+        required: true
+      },
+      edad: {
+        type: Number,
+        min: 0,
+        max: 120,
+        required: true
+      },
+      genero: {
+        type: String,
+        enum: ['F', 'M'],
+        required: true
+      }
+});
+
+let hotelSchema = mongoose.Schema({
+    nombre: {
+      type: String,
+      required: true
+    },
+    ubicacion: {
+      type: String,
+      required: true
+    },
+    categoria: {
+      type: String,
+      required: true
+    },
+    servicios: {
+      type: [String],
+      required: true
+    },
+    habitaciones: {
+      individuales: {
+        type: Number,
+        required: true
+      },
+      dobles: {
+        type: Number,
+        required: true
+      },
+      suites: {
+        type: Number,
+        required: true
+      }
+    },
+    contacto: {
+      telefono: {
+        type: String,
+        required: true
+      },
+      correo: {
+        type: String,
+        required: true
+      }
+    }
+  });
+
+  const users = mongoose.model('users', userSchema);
+  const hotels = mongoose.model('hotels', hotelSchema);
+  */
+/*
+
+
+const user = mongoose.model('User', userSchema);
+
+app.post('/api/users', async (req, res) => {
+    try {
+        const userData = req.body;
+        const newUser = new user(userData);
+        await newUser.save();
+        res.status(201).json(newUser);
+        newUser.save().then((doc) => console.log(("User created: ") + doc));
+    } catch (error) {
+        res.status(400).json({ error: 'ERROR' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server on port: ${port}`);
+});
+*/
 
 /*import express from "express"
 import cors from "cors"
