9c67d34eac31a505d3b516405fccee067233a41e
Francisco Flores <is728238@iteso.mx>
2023-11-21 10:08:20 -0600

    Login Terminado

---
+++ login.js
@@ -0,0 +1,55 @@
+function iniciarSesion() {
+    // Mostrar el spinner de carga
+    document.getElementById("loadingSpinner").style.display = "inline-block";
+
+    // Recolectar datos del formulario
+    const usuario = document.getElementById("usuario").value;
+    const contrasenia = document.getElementById("password").value;
+
+    // Enviar datos al servidor
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/login",
+        data: {
+            usuario,
+            contrasenia
+        },
+        success: function (response) {
+            console.log("Inicio de sesión exitoso:", response);
+        },
+        error: function (error) {
+            console.error("Error al iniciar sesión:", error);
+        },
+        complete: function () {
+            document.getElementById("loadingSpinner").style.display = "none";
+        }
+    });
+}
+
+
+
+function registrarUsuario() {
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const contrasenia = document.getElementById("newpassword").value;
+    const edad = document.getElementById("edad").value;
+    const genero = document.getElementById("genero").value;
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/register",
+        data: {
+            nombre,
+            correo,
+            contrasenia,
+            edad,
+            genero
+        },
+        success: function (response) {
+            console.log("Usuario registrado con éxito:", response);
+        },
+        error: function (error) {
+            console.error("Error al registrar usuario:", error);
+        }
+    });
+}
