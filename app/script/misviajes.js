cfdc96e7bd048d27a356e4d6e5ded5ba22eaf650
codrenatat <codrenatat@gmail.com>
2023-11-23 07:18:49 -0600

    Commit de yami

--- app/script/misviajes.js
+++ app/script/misviajes.js
@@ -3,9 +3,12 @@ function showMisViajes(viajes = []) {
 
   tablaViajes.innerHTML = "";
 
+  let totalGeneral = 0;
+
   viajes.forEach((viaje) => {
     let total = Number(viaje.numNoches || 1) * Number(viaje.total) * Number(viaje.numPersonas || 1);
-    console.log(viaje);
+    totalGeneral += total;
+
     const fila = document.createElement("tr");
 
     const idViaje = document.createElement("td");
@@ -46,20 +49,82 @@ function showMisViajes(viajes = []) {
     fila.appendChild(numPersonas);
 
     const totalEl = document.createElement("td");
+    totalEl.className = "total";
     totalEl.textContent = total;
     fila.appendChild(totalEl);
 
+    const eliminarBtn = document.createElement("button");
+    eliminarBtn.textContent = "Eliminar";
+    eliminarBtn.className = "btn btn-danger";
+    eliminarBtn.addEventListener("click", () => eliminarViaje(viaje._id, total, viaje.hotelId));
+    const eliminarTd = document.createElement("td");
+    eliminarTd.appendChild(eliminarBtn);
+    fila.appendChild(eliminarTd);
+
+    fila.setAttribute("data-id", viaje._id);
+
     tablaViajes.appendChild(fila);
   });
+
+  const totalFila = document.createElement("tr");
+  totalFila.innerHTML = `
+    <td colspan="3"></td>
+    <td class="totalLabel">Total</td>
+    <td id="totalViajes" class="total" colspan="3">${totalGeneral}</td>
+    <td></td>
+  `;
+  tablaViajes.appendChild(totalFila);
 }
 
-function recalculateTotal(event) {
-  const row = event.target.closest("tr");
-  const numNoches = Number(row.querySelector(".numNoches").value);
-  const numPersonas = Number(row.querySelector(".numPersonas").value);
-  const precioxnoche = Number(row.querySelector(".precioxnoche").innerText);
-  const total = numNoches * precioxnoche * numPersonas;
-  row.querySelector("td:last-child").textContent = total;
+function recalculateTotal() {
+  let totalGeneral = 0;
+  const filas = document.querySelectorAll("#tablaViajes tr:not(:last-child)");
+
+  filas.forEach((fila) => {
+    const numNoches = Number(fila.querySelector(".numNoches").value);
+    const numPersonas = Number(fila.querySelector(".numPersonas").value);
+    const precioxnoche = Number(fila.querySelector(".precioxnoche").textContent);
+    const total = numNoches * precioxnoche * numPersonas;
+    fila.querySelector(".total").textContent = total;
+    totalGeneral += total;
+  });
+
+  document.getElementById("totalViajes").textContent = totalGeneral;
+}
+
+function eliminarViaje(idViaje, total, hotelId) {
+  const filaAEliminar = document.querySelector(`#tablaViajes tr[data-id="${idViaje}"]`);
+  filaAEliminar.remove();
+  recalculateTotal();
+  guardarViajesEnAlmacenamientoLocal();
+}
+
+function guardarViajesEnAlmacenamientoLocal() {
+  const filas = document.querySelectorAll("#tablaViajes tr[data-id]");
+  const viajes = [];
+
+  filas.forEach((fila) => {
+    const idViaje = fila.getAttribute("data-id");
+    const playa = fila.querySelector("td:nth-child(2)").textContent;
+    const hotel = fila.querySelector("td:nth-child(3)").textContent;
+    const precioxnoche = fila.querySelector(".precioxnoche").textContent;
+    const numNoches = fila.querySelector(".numNoches input").value;
+    const numPersonas = fila.querySelector(".numPersonas input").value;
+    const total = fila.querySelector(".total").textContent;
+
+    viajes.push({
+      _id: idViaje,
+      playa: playa,
+      hotel: hotel,
+      total: precioxnoche,
+      numNoches: numNoches,
+      numPersonas: numPersonas,
+      
+      total: total,
+    });
+  });
+
+  localStorage.setItem("viajes", JSON.stringify(viajes));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -70,9 +135,7 @@ async function obtenerYMostrarMisViajes() {
   try {
     const usuarioId = "usuario123";
 
-    const response = await fetch(
-      `http://localhost:3000/viajes?usuario=${usuarioId}`
-    );
+    const response = await fetch(`http://localhost:3000/viajes?usuario=${usuarioId}`);
     const viajes = await response.json();
     showMisViajes(viajes);
   } catch (error) {
